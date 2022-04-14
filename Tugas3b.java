216773b263b8e3adbcf6208b5ce9277424612df2
Ria Kusmara <89031250+riakusmaraa@users.noreply.github.com>
2022-04-14 09:57:16 +0700

    Add files via upload

---
+++ Tugas3b.java
@@ -0,0 +1,20 @@
+public class Tugas3b {
+    public static void main(String[] args) {
+        //input
+        int uts = 75;
+        int uas = 80;
+        int quiz1 = 95;
+        int quiz2 = 88;
+        int quiz3 = 90;
+        int ukk;
+        double NA;
+        
+        //proses
+        ukk = (quiz1+quiz2+quiz3)/3;
+        NA = (0.15*uts)+(0.15*uas)+(0.7*ukk);
+        
+        //output
+        System.out.println("Nilai Akhir Anda: "+ String.format("%.2f",NA));
+    
+}
+}
